6940ed9b31f8c9e6f04a35fbde108177641438bc
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 10:46:08 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skriv ut en slumpad tipskupong (13 rader med 1, X, 2)
+        // Använd gärna Math.random
+
+
+    }
+}
