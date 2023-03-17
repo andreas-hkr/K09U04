b56a13b0a408f301f7973c5ceb86530ea73eceda
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 10:55:45 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,15 @@ public class Main {
         // Skriv ut en slumpad tipskupong (13 rader med 1, X, 2)
         // Använd gärna Math.random
 
-
+        for (int i=0; i < 13; i++) {
+            int x = (int)(Math.random() * 3);
+            if (x == 0) {
+                System.out.println("1");
+            } else if(x == 1) {
+                System.out.println(" X");
+            } else {
+                System.out.println("  2");
+            }
+        }
     }
 }
